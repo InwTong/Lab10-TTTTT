b354b207130f538176352ee7c8a7453904c86587
InwTong <Tachin_s@cmu.ac.th>
2024-12-17 14:26:26 +0700

    Update lab10_1.cpp

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,26 +2,45 @@
 using namespace std;
 
 int main(){
-	int count[5] = {}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
-	cout << "Please input grade of each student (A-F) or input 0 to exit.";
-	do{
-		cout << "Student [" << "]: ";
-		cin >> grade; //The loop must be terminated when grade = '0'
-		if(true) // if grade is A
-			//Do something
-		}else if(true) // if grade is B
-			//Do something
-		//and so on ... for grade = C, D, F	
-		}else{ // grade is wrong input
-			//Do something
-		} 
-	}while(true);
-	
-	
-	cout << "In total ? students.";
-	cout << "A = " << count[0] <<", ";
-	cout << "B = " << count[1] <<", ";	
-	//	and so on ... for grade = C, D, F	
-	
-	return 0;
+    int count[5] = {};  // Declare array count for counting A, B, C, D, F and initialize all elements to 0
+    char grade;
+    int studentCount = 0; // Variable to count the number of students entered
+
+    cout << "Please input grade of each student (A-F) or input 0 to exit." << endl;
+
+    // Loop to get grades of students
+    do {
+        studentCount++;  // Increment student count for each new entry
+        cout << "Student [" << studentCount << "]: ";
+        cin >> grade; // Input the grade
+
+        // Check the grade and update the corresponding count
+        if (grade == 'A') {
+            count[0]++;
+        } else if (grade == 'B') {
+            count[1]++;
+        } else if (grade == 'C') {
+            count[2]++;
+        } else if (grade == 'D') {
+            count[3]++;
+        } else if (grade == 'F') {
+            count[4]++;
+        } else if (grade == '0') {
+            break;  // Exit the loop when the input is '0'
+        } else {
+            // Handle incorrect input
+            cout << "Wrong input. Please input again." << endl;
+            studentCount--;  // Do not count this entry as it's invalid
+        }
+    } while (true);
+
+    // Output the results
+    cout << "In total " << studentCount - 1 << " students." << endl; // Subtract 1 to exclude the '0' input
+    cout << "A = " << count[0] << ", ";
+    cout << "B = " << count[1] << ", ";
+    cout << "C = " << count[2] << ", ";
+    cout << "D = " << count[3] << ", ";
+    cout << "F = " << count[4] << endl;
+
+    return 0;
 }
